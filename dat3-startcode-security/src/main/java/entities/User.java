e0365ae8411795d125a8789e65a33fe944c2b8fb
atombomben <71074932+atombomben@users.noreply.github.com>
2021-11-11 13:49:19 +0100

    updated bcrypt
    updated entity

--- dat3-startcode-security/src/main/java/entities/User.java
+++ dat3-startcode-security/src/main/java/entities/User.java
@@ -52,14 +52,15 @@ public class User implements Serializable {
   public User() {}
 
   //TODO Change when password is hashed
-   public boolean verifyPassword(String pw){
-        return(pw.equals(userPass));
-    }
-   
+  public boolean verifyPassword(String pw){
+    return(BCrypt.checkpw(pw,userPass));
+  }
+
   public User(String userName, String userPass) {
     this.userName = userName;
-    this.userPass = userPass;
+    this.userPass = BCrypt.hashpw(userPass,BCrypt.gensalt());
   }
+  
    public User(UserDTO u) {
        // if (u.getId() != null) this.id = u.getId();
         this.userName = u.getUserName();
