0da3cd47e88f64bfd86eb3f287204c35e1b06328
Oliver Juul Reder <oliver.j.reder@gmail.com>
2021-11-14 15:56:06 +0100

    Added CountryDTO

---
+++ dat3-startcode-security/src/main/java/dtos/CountryDTO.java
@@ -0,0 +1,21 @@
+package dtos;
+
+public class CountryDTO {
+    private String capital;
+    private String population;
+
+
+    private class flags {
+        private String png;
+    }
+
+    private class name {
+        private String common;
+        private String official;
+    }
+
+    private class currencies {
+        private String DKK;
+    }
+
+}
